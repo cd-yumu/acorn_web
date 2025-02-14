6f28d12c6f0ac1f34fadf8a2f0b45b7b17c776f9
EugeneChoi <yoojin7526@naver.com>
2025-02-14 12:41:12 +0900

    Spring01_Hello 스프링 프레임워크 프로젝트 생성 및 Bean 객체 예제

---
+++ Spring01_Hello/src/main/java/com/example/spring01/serveice/Drill.java
@@ -0,0 +1,5 @@
+package com.example.spring01.serveice;
+
+public interface Drill {
+	public void hole();
+}
---
+++ Spring01_Hello/src/main/java/com/example/spring01/serveice/HomeServiceImpl.java
@@ -0,0 +1,45 @@
+package com.example.spring01.serveice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/*
+ * 스프링 프레임 워크가 HomeServiceImpl 클래스가 존재하는 패키지 (com.example.spring01.service) 를
+ * component scan 을 하게 되면 적절한 어노테이션(@Component)이 붙어 있는 
+ * 이 클래스로 객체를 생성해서 해당 객체의 참조값을 spring bean container 에서 관리하게 된다.
+ * 
+ * - 어디를 scan 하게 되나?
+ * 	@SpringApplication 어노테이션이 붙어있는 클래스가 존재하는 페키지 혹은 하위 페키지를 모두 scan 한다.
+ * 
+ */
+
+@Component
+public class HomeServiceImpl implements HomeService {
+
+	// 의존 객체 주입받기(DI)
+	@Autowired Drill drill;
+	
+	// 생성자
+	public HomeServiceImpl() {
+		System.out.println("HomeServiceImpl 객체가 생성됨");
+	}
+	
+	@Override
+	public void clean(String name) {
+		System.out.println(name+" 의 집을 청소하세요!");
+	}
+
+	@Override
+	public void wash(String name) {
+		System.out.println(name+" 의 빨래를 빨아요~");
+	}
+
+	@Override
+	public void hole(String name) {
+		System.out.println(name+" 의 벽에 구멍을 뚫어요");
+		drill.hole();
+	}
+	
+	
+	
+}
