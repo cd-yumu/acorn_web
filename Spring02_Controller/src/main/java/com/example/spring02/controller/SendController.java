803d51d2a13ccc370bea824c5574dc43b147fd2c
EugeneChoi <yoojin7526@naver.com>
2025-02-17 10:16:38 +0900

    Spring02_Controller 요청 파라미터 추출 예제 추가

--- Spring02_Controller/src/main/java/com/example/spring02/controller/SendController.java
+++ Spring02_Controller/src/main/java/com/example/spring02/controller/SendController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.spring02.dto.MemberDto;
+
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
@@ -36,4 +38,24 @@ public class SendController {
 	
 	//@ResponseBody 이 어노테이션을 붙이면 바로 응답한다.
 	// 없다면 view page 에 정보를 담아 응답한다.
+	
+	
+	
+	@ResponseBody
+	@PostMapping("/send3")
+	public String send3(int num, String name, String addr) {
+		System.out.println(num+" | "+name+" | "+addr);
+		return "/send3 okay!";
+	}
+	
+	/*
+	 * 매개 변수에 dto 를 선언하면 추출된 요청 파라미터가 dto 객체에 담겨서 전달된다.
+	 */
+	@ResponseBody
+	@PostMapping("/send4")
+	public String send4(MemberDto dto) {
+		System.out.printf("%d | %s | %s",dto.getNum(), dto.getName(), dto.getAddr());
+		
+		return "/send4 okay!";
+	}
 }
