e22fae36a0bbeb42da4fc868ff3acd3eb8d91698
EugeneChoi <yoojin7526@naver.com>
2025-02-19 17:08:24 +0900

    Spring06_Service alias 별칭 부여 추가

--- Spring06_Service/src/main/java/com/example/spring06/dto/MemberDto.java
+++ Spring06_Service/src/main/java/com/example/spring06/dto/MemberDto.java
@@ -1,8 +1,16 @@
 package com.example.spring06.dto;
 
+import org.apache.ibatis.type.Alias;
+
 import lombok.Getter;
 import lombok.Setter;
 
+
+/*
+ * type 에 별칭을 부여하고 Mapper xml 문서에서 별칭을 이용해서 
+ * parameterType 과 resultType 을 설정할 수 있다.
+ */
+@Alias("memberDto")
 @Setter
 @Getter
 public class MemberDto {
