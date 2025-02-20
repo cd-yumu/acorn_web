86f31d14dbd8742cd41a7989ed2ed96b3480ef6f
Eugene Choi <yoojin7526@naver.com>
2025-02-20 23:41:18 +0900

    MyTest05 - 파일 업로드 다운로드 연습

--- MyTest05/src/main/java/com/example/mytest05/MyTest05Application.java
+++ MyTest05/src/main/java/com/example/mytest05/MyTest05Application.java
@@ -2,7 +2,11 @@ package com.example.mytest05;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.event.EventListener;
 
+@PropertySource(value="classpath:custom.properties")
 @SpringBootApplication
 public class MyTest05Application {
 
@@ -10,4 +14,25 @@ public class MyTest05Application {
 		SpringApplication.run(MyTest05Application.class, args);
 	}
 
+	// 서버가 준비 되었을 때 실행할 메소드 설정
+	@EventListener(ApplicationReadyEvent.class)
+	public void openChrome() {
+		String url = "http://localhost:9000/mytest05/";
+		// 운영체젝의 이름을 소문자로
+		String os = System.getProperty("os.name").toLowerCase();
+		ProcessBuilder builder = null;
+		try {
+			if (os.contains("win")) {
+				builder = new ProcessBuilder("cmd.exe", "/c", "start chrome " + url);
+			} else if (os.contains("mac")) {
+				builder = new ProcessBuilder("/usr/bin/open", "-a", "Google Chrome", url);
+			} else {
+				System.out.println("지원하지 않는 운영체제 입니다.");
+				return;
+			}
+			builder.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
