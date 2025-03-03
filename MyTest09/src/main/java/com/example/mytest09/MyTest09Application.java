86fb654118efdef1bea0b357a9e6a251027a1599
Eugene Choi <yoojin7526@naver.com>
2025-03-03 12:13:29 +0900

    mytest 새 브랜치에 MyTest09 프로젝트 추가

---
+++ MyTest09/src/main/java/com/example/mytest09/MyTest09Application.java
@@ -0,0 +1,13 @@
+package com.example.mytest09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTest09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTest09Application.class, args);
+	}
+
+}
---
+++ MyTest09/src/test/java/com/example/mytest09/MyTest09ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mytest09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTest09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
